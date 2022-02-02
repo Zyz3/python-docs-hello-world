46e460057672597eae9f21c0ecc9a318ac7e6caf
Zyz3 <98898182+Zyz3@users.noreply.github.com>
2022-02-02 16:22:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Labukas katyte, ka vakare? :)****"
+    return "Labukas katyte, ka vakare? :)****"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.mizu.com%2Fi-love-you-quotes&psig=AOvVaw28HSTcGvs4jiOnSXKFcHd8&ust=1643905305249000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJjbotC24fUCFQAAAAAdAAAAABAG
